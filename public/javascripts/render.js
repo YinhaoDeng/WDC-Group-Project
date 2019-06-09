46467f4cc3c3fe018550ee4fc2181fc90e2455eb
Yinhao <1454135273@qq.com>
2019-06-09 19:20:45 +0930

    big update

---
+++ public/javascripts/render.js
@@ -0,0 +1,11 @@
+function load() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("posts").innerHTML =
+            this.responseText;
+        }
+    };
+    xhttp.open("GET", "/chrisinfo", true);
+    xhttp.send();
+}
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var chris = require("./db")
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get("/chrisinfo", function(req, res, next){
+  chris.connect();
+
+  conn.query('SELECT * FROM restaurant_detail;', function (error, results, fields) {
+    if (error) throw error;
+    console.log(results);
+    res.send(results);
+  });
+
+  chris.end();
 });
 
 module.exports = router;
