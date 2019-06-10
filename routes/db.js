0a5f2264b763cbc8247dc69538473e3403314346
ZixianLu <50563989+ZixianLu@users.noreply.github.com>
2019-06-10 16:12:41 +0930

    Update db.js

--- routes/db.js
+++ routes/db.js
@@ -1,8 +1,8 @@
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
     host     : 'localhost',
-    password : 'DYH981209'
-    database : 'chris'
+    password : 'DYH981209',
+    database : 'wdc_project_database'
 });
 
 module.exports = connection;
