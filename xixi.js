522a3f3f72fe22e2b28df2c2dc3309075e9ca02c
BohuiGao <50566036+BohuiGao@users.noreply.github.com>
2019-06-11 14:11:35 +0930

    Add files via upload

---
+++ xixi.js
@@ -0,0 +1,15 @@
+
+function signIn()
+{
+	$(".data").css("display","block");
+	$(".g-signin2").css("display","none");
+
+
+}
+
+function signOut()
+{
+	
+$(".data").css("display","none");
+$(".g-signin2").css("display","block");
+}
